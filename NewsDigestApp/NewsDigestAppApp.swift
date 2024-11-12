d9af58162ca627ebbb3f2f84bb4ec4631167b564
khamitov527 <khamitov527@gmail.com>
2024-11-12 11:26:54 -0500

    Initial Commit

---
+++ NewsDigestApp/NewsDigestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDigestAppApp.swift
+//  NewsDigestApp
+//
+//  Created by Akylbek Khamitov on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsDigestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsDigestAppTests/NewsDigestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDigestAppTests.swift
+//  NewsDigestAppTests
+//
+//  Created by Akylbek Khamitov on 11/12/24.
+//
+
+import Testing
+@testable import NewsDigestApp
+
+struct NewsDigestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
