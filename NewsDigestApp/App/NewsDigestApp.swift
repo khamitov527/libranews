0ac96323c8f82a89def23158a1138154d254b902
khamitov527 <khamitov527@gmail.com>
2024-11-13 16:01:29 -0500

    junk removed

--- NewsDigestApp/App/NewsDigestApp.swift
+++ NewsDigestApp/App/NewsDigestApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct NewsDigestApp: App {
-    @StateObject private var userPreferences = UserPreferences()
-    
     var body: some Scene {
         WindowGroup {
             HomeView()
-                .environmentObject(userPreferences)
         }
     }
 }
