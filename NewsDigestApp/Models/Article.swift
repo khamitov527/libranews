a5046787d821cc0511e378f59f116d2ecc8f24ab
khamitov527 <khamitov527@gmail.com>
2024-11-23 12:13:30 -0500

    cleanup

--- NewsDigestApp/Models/Article.swift
+++ NewsDigestApp/Models/Article.swift
@@ -37,51 +37,51 @@ struct Article: Identifiable, Codable, Hashable {
     }
     
     // uncomment init to test with dummy data
-    init(source: ArticleSource, author: String?, title: String, description: String?, url: String?, urlToImage: String?, publishedAt: Date?, content: String?) {
-        self.source = source
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
-    }
+//    init(source: ArticleSource, author: String?, title: String, description: String?, url: String?, urlToImage: String?, publishedAt: Date?, content: String?) {
+//        self.source = source
+//        self.author = author
+//        self.title = title
+//        self.description = description
+//        self.url = url
+//        self.urlToImage = urlToImage
+//        self.publishedAt = publishedAt
+//        self.content = content
+//    }
     //
     
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        self.title = try container.decode(String.self, forKey: .title)
-//        self.description = try container.decodeIfPresent(String.self, forKey: .description)
-//        self.content = try container.decodeIfPresent(String.self, forKey: .content)
-//        self.source = try container.decode(ArticleSource.self, forKey: .source)
-//        self.author = try container.decodeIfPresent(String.self, forKey: .author)
-//        self.url = try container.decodeIfPresent(String.self, forKey: .url)
-//        self.urlToImage = try container.decodeIfPresent(String.self, forKey: .urlToImage)
-//        
-//        // Parse the date string
-//        if let dateString = try container.decodeIfPresent(String.self, forKey: .publishedAt) {
-//            let dateFormatter = ISO8601DateFormatter()
-//            self.publishedAt = dateFormatter.date(from: dateString)
-//        } else {
-//            self.publishedAt = nil
-//        }
-//    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.content = try container.decodeIfPresent(String.self, forKey: .content)
+        self.source = try container.decode(ArticleSource.self, forKey: .source)
+        self.author = try container.decodeIfPresent(String.self, forKey: .author)
+        self.url = try container.decodeIfPresent(String.self, forKey: .url)
+        self.urlToImage = try container.decodeIfPresent(String.self, forKey: .urlToImage)
+        
+        // Parse the date string
+        if let dateString = try container.decodeIfPresent(String.self, forKey: .publishedAt) {
+            let dateFormatter = ISO8601DateFormatter()
+            self.publishedAt = dateFormatter.date(from: dateString)
+        } else {
+            self.publishedAt = nil
+        }
+    }
     
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(title, forKey: .title)
-//        try container.encode(source, forKey: .source)
-//        try container.encodeIfPresent(description, forKey: .description)
-//        try container.encodeIfPresent(content, forKey: .content)
-//        try container.encodeIfPresent(author, forKey: .author)
-//        try container.encodeIfPresent(url, forKey: .url)
-//        try container.encodeIfPresent(urlToImage, forKey: .urlToImage)
-//        if let publishedAt = publishedAt {
-//            let dateFormatter = ISO8601DateFormatter()
-//            try container.encode(dateFormatter.string(from: publishedAt), forKey: .publishedAt)
-//        }
-//    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(source, forKey: .source)
+        try container.encodeIfPresent(description, forKey: .description)
+        try container.encodeIfPresent(content, forKey: .content)
+        try container.encodeIfPresent(author, forKey: .author)
+        try container.encodeIfPresent(url, forKey: .url)
+        try container.encodeIfPresent(urlToImage, forKey: .urlToImage)
+        if let publishedAt = publishedAt {
+            let dateFormatter = ISO8601DateFormatter()
+            try container.encode(dateFormatter.string(from: publishedAt), forKey: .publishedAt)
+        }
+    }
 }
 
 struct ArticleSource: Codable, Hashable {
